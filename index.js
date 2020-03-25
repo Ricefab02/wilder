0edc1fa5ef132df144df6089891ac227245bbed5
Ricefab02 <roger.fabrice02@gmail.com>
2020-03-25 16:15:28 +0100

    up index

---
+++ index.js
@@ -0,0 +1,10 @@
+const chalk = require('chalk');
+const name = ["Angélique", "Anthony", "Lucas", "romain"]
+
+console.log(chalk.red (name[0]));
+console.log(chalk.blue (name[1]));
+console.log(chalk.green (name[2]));
+console.log(chalk.yellow (name[3]));
+    
+ 
+ 
